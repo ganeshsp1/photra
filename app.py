9e1ecadb43cb8fc3dd76c415a4cbc7d32489e07f
ganeshsp1 <ganesh.sp006@gmail.com>
2017-09-14 16:14:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def makeWebhookResult(data):
 
     return {
         "speech": speech,
-        "displayText":"Yo Yo Ganesh" ,
+        "displayText":speech ,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
