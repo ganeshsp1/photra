57e419d901f14c95473fa5c7f913585651ce963c
ganeshsp1 <ganesh.sp006@gmail.com>
2017-09-18 14:24:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-def cityName
+cityName="test"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
