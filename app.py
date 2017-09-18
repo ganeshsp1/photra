f892c52193fe3f7b2e066b6e5b2138aa08cf3e01
ganeshsp1 <ganesh.sp006@gmail.com>
2017-09-18 13:25:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,10 +97,10 @@ def makeWebhookResult(data):
     if newResults is None:
         return {}
     photosList = "tets"
-	#newResults = json.dumps(newResults, indent=4)
-    for d in newResults:
-		yql_url1 = baseurl + str(json.dumps(d, indent=4))
-        photos = d.get('photos')
+	newResults = json.dumps(newResults, indent=4)
+	yql_url1 = newResults
+    #for d in newResults:
+     #   photos = d.get('photos')
         #for photo in photos:
 			#photosList = photos[ 0 ].get('photo_reference')
     baseurl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyCXLMsw0sL_TrHjtgR7DjEM3gHKb5QnJzs&photoreference="
